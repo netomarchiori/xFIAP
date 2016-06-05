666dd65f8b3c5814fe47cc2a55e7ed4649a78015
Mauro Marchiori <mauro@marchiorineto.com.br>
2016-06-05 01:55:04 -0300

    produto detalhe - inicial

--- xFIAP/View/ProdutoPage.xaml.cs
+++ xFIAP/View/ProdutoPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using xFIAP.Model;
 using xFIAP.ViewModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -21,6 +22,17 @@ namespace xFIAP.View
         private void ProdutoView_Loaded(object sender, RoutedEventArgs e)
         {
             this.DataContext = App.ProdutoVM;
+        }
+
+        private void lstProdutos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ProdutoModel selectedProduct = (sender as ListBox).SelectedItem as ProdutoModel;
+
+            if (selectedProduct != null)
+            {
+                System.Diagnostics.Debug.WriteLine(selectedProduct.Descricao);
+                Frame.Navigate(typeof(ProdutoDetalhe),selectedProduct);
+            }
         }
     }
 }
---
+++ xFIAP/ViewModel/ProdutoDetalheViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xFIAP.Model;
+
+namespace xFIAP.ViewModel
+{
+    public class ProdutoDetalheViewModel
+    {
+
+        public ProdutoModel produto;
+
+        public ProdutoDetalheViewModel()
+        {
+            this.InitializeVM();
+        }
+
+        private void InitializeVM()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
