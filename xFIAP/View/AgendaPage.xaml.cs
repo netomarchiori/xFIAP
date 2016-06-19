13ce39ed8559234a635ae351bb05f267a19ab460
Mauro Marchiori <mauro@marchiorineto.com.br>
2016-06-19 16:49:35 -0300

    pagina de clientes

--- xFIAP/View/AgendaPage.xaml.cs
+++ xFIAP/View/AgendaPage.xaml.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Appointments;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Maps;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -66,6 +69,27 @@ namespace xFIAP.View
             Termino = dteTermino.Date.Value.AddHours(tmeTermino.Time.Hours).AddMinutes(tmeTermino.Time.Minutes);
 
             return (dteTermino.Date.Value - dteInicio.Date.Value);
+        }
+
+        private async void btnRota_Click(object sender, RoutedEventArgs e)
+        {
+            string address = txtLocalizacao.Text;
+
+            var locFinderResult = await MapLocationFinder.FindLocationsAsync(address, new Geopoint(new BasicGeoposition()));
+
+            // add error checking here
+
+            var geoPos = locFinderResult.Locations[0].Point.Position;
+
+            var routeToUri = new Uri(String.Format(
+                 "ms-drive-to:?destination.latitude={0}&destination.longitude={1}&destination.name={2}",
+                 geoPos.Latitude,
+                 geoPos.Longitude,
+                 address));
+
+            var launcherOptions = new LauncherOptions();
+            launcherOptions.TargetApplicationPackageFamilyName = "Microsoft.WindowsMaps_8wekyb3d8bbwe";
+            await Launcher.LaunchUriAsync(routeToUri, launcherOptions);
         }
     }
 }
